e444d705e96a8c44905af73b673d14081b25a689
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-01-30 11:06:04 -0500

    single extensions class

---
+++ Extensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace System
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Invoke an Action delegate or event in a thread-safe manner. By passing the delegate
+        /// as a method parameter, modifications to the original (such as unsubscribing all handlers)
+        /// will not affect the local copy.
+        /// </summary>
+        public static void SafeInvoke(this Action action)
+        {
+            if(action == null) 
+                return;
+            
+            action();
+        }
+
+        public static void SafeInvoke<T>(this Action<T> action, T obj)
+        {
+            if(action == null) 
+                return;
+            
+            action(obj);
+        }
+
+        public static void SafeInvoke(this EventHandler eventHandler, object sender, EventArgs args)
+        {
+            if(eventHandler == null) 
+                return;
+            
+            eventHandler(sender, args);
+        }
+
+        public static void SafeInvoke<T>(this EventHandler<T> eventHandler, object sender, T args)
+            where T : EventArgs
+        {
+            if(eventHandler == null) 
+                return;
+            
+            eventHandler(sender, args);
+        }
+
+        public static void SafeInvoke(this NotifyCollectionChangedEventHandler eventHandler, object sender, NotifyCollectionChangedEventArgs args)
+        {
+            if (eventHandler == null)
+                return;
+
+            eventHandler(sender, args);
+        }
+
+        public static void SafeInvoke(this PropertyChangedEventHandler eventHandler, object sender, PropertyChangedEventArgs args)
+        {
+            if (eventHandler == null)
+                return;
+
+            eventHandler(sender, args);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SafeEvents")]
+[assembly: AssemblyProduct("SafeEvents")]
+[assembly: AssemblyCopyright("Copyright © Anthony Mastrean 2013")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#elif RELEASE
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
