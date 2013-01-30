bbb7d10abe96de352b71a5c548bae02f3a5d4da3
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-01-30 11:11:19 -0500

    reordered the source

--- SafeEvents/Properties/AssemblyInfo.cs
+++ SafeEvents/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("SafeEvents")]
+[assembly: AssemblyDescription("Invoke an Action delegate or event in a thread-safe manner.")]
 [assembly: AssemblyProduct("SafeEvents")]
 [assembly: AssemblyCopyright("Copyright © Anthony Mastrean 2013")]
 [assembly: AssemblyVersion("1.0.0.0")]
